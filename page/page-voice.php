4e70fa427180ab4c5ebd544d705e6f7e15017a4a
今井美奈 <106499852+mina0929@users.noreply.github.com>
2025-10-08 16:17:50 +0900

    OCのテンプレートを用意

--- page/page-voice.php
+++ page/page-voice.php
@@ -1,80 +1,86 @@
 <section class="p-voice" id="voice">
-    <div class="p-accessmap__inner c-sec">
-        <div class="p-accessmap__content">
-            <!-- Overview -->
-            <div class="p-accessmap__overview">
-                <figure class="u-flex u-items-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/lower/accessmap/accessmap_img01.png"
-                        alt="東京墨田看護専門学校 外観イメージ" class="p-accessmap__image">
-                    <figcaption class="p-accessmap__caption">
-                        <h2 class="p-accessmap__school">東京墨田看護専門学校</h2>
-                        <div class="p-accessmap__address u-mt20">
-                            〒131-0032 東京都墨田区東向島5-6-6
-                            <a href="" class="c-btn__link" target="_blank" rel="noopener">Google Map</a>
-                        </div>
-
-                        <div class="p-accessmap__tel u-mt20">
-                            <a href="tel:0120-901-290" aria-label="電話 0120-901-290">0120-901-290</a>
-                        </div>
-
-                        <p class="p-accessmap__note">
-                            スカイツリーのすぐそばにある学校です！
-                        </p>
-                    </figcaption>
-                </figure>
-
-                <div class="p-accessmap__map">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.013929978177!2d139.8186208155518!3d35.725875935129004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188f22708151fb%3A0x30b3ea487ed11128!2z44CSMTMxLTAwMzIg5p2x5Lqs6YO95aKo55Sw5Yy65p2x5ZCR5bO277yV5LiB55uu77yW4oiS77yW!5e0!3m2!1sja!2sjp!4v1592806731974!5m2!1sja!2sjp"
-                        class="u-w100" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen=""
-                        loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="東京墨田看護専門学校の地図">
-                    </iframe>
-                </div>
-            </div>
-
-            <!-- Access -->
-            <div class="p-accessmap__access">
-                <h2 class="c-sec__title">
-                    <span class="c-sec__title--en">ACCESS</span><span class="c-sec__title--ja">アクセス</span>
-                </h2>
-
-                <figure class="u-flex u-items-center">
-                    <figcaption class="p-accessmap__access-info">
-                        <p>
-                            東武スカイツリーライン（東武伊勢崎線）<br>
-                            「東向島駅」より徒歩2分
-                        </p>
-
-                        <div class="p-accessmap__time-list" aria-label="各地域からの所要時間">
-                            <i>各地域からの所要時間</i>
-                            <ul>
-                                <li>北千住8分</li>
-                                <li>錦糸町11分</li>
-                                <li>松戸24分</li>
-                                <li>柏33分</li>
-                                <li>船橋33分</li>
-                                <li>池袋36分</li>
-                                <li>春日部38分</li>
-                                <li>新宿42分</li>
-                                <li>品川44分</li>
-                                <li>千葉47分</li>
-                                <li>大宮53分</li>
-                                <li>つくば63分</li>
-                                <li>横浜64分</li>
-                                <li>立川75分</li>
-                            </ul>
-                        </div>
-
-                        <p>
-                            東武スカイツリーライン「東向島」駅の改札を出て、くすりの福太郎を正面に見て、右に少し進むとマクドナルドが見えるので、マクドナルドを右手に真っすぐ進みます。しばらく進むとファミリーマートが見えてきますので、左に進んでください。左手に東京墨田看護専門学校があります。
-                        </p>
-                    </figcaption>
-
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/lower/accessmap/accessmap_img02.png"
-                        alt="学校までのアクセス案内図" class="p-accessmap__access-image">
-                </figure>
-            </div>
-
+    <p class="u-text-center">「オープンキャンパス」と「入試説明会」に参加していただいた方々からの感想をご紹介します！</p>
+    <div class="p-voice__btnbox u-grid u-col-2">
+        <div class="p-voice__btn"><a href=""
+                class="c-btn__link c-btn__link-border c-btn__link-anchor">オープンキャンパス参加者の声</a></div>
+        <div class="p-voice__btn"><a href="" class="c-btn__link c-btn__link-border c-btn__link-anchor">一覧を見る</a>
         </div>
     </div>
+
+
+    <div class="p-voice__sec c-sec">
+        <h2 class="c-sec__title c-sec__title--center c-sec__title--big"><span
+                class="c-sec__title--ja">オープンキャンパス参加者の声</span></h2>
+        <ul class="p-voice__list u-grid u-col-3">
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem --dgreen">
+                <h3 class="p-voice__listtitle --dgreen">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+        </ul>
+    </div>
+
+
+    <div class="p-voice__sec c-sec">
+        <h2 class="c-sec__title c-sec__title--center c-sec__title--big"><span
+                class="c-sec__title--ja">オープンキャンパス参加者の声</span></h2>
+        <ul class="p-voice__list u-grid u-col-3">
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem --dgreen">
+                <h3 class="p-voice__listtitle --dgreen">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+            <li class="p-voice__listitem">
+                <h3 class="p-voice__listtitle">学校生活や入試情報を詳しく<br>聞けて大満足でした</h3>
+                <p class="p-voice__listtxt">学校生活についての話をたくさん聞くことができて楽しかったです。入試についての情報も色々と聞けて、またオープンキャンパスに参加したいと思いました。</p>
+                <small class="p-voice__listname">Aさん　高校三年生</small>
+            </li>
+        </ul>
+    </div>
+
 </section>
