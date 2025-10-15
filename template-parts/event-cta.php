305b9fc3a5942bb85aadb9c29eb9f1da9f83712a
nakano <nakai@double-factory.com>
2025-10-15 15:31:29 +0900

    nakaitesuto

--- template-parts/event-cta.php
+++ template-parts/event-cta.php
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-<section>
-    <div class="l-container">
-        <div class="c-cta">
-            <a href="" class="c-cta__link">
-                <div class="c-cta__img"><img
-                        src="<?php echo get_template_directory_uri(); ?>/assets/images/common/cta.png" alt="イベント情報">
-                </div>
-=======
 <section class="c-event-cta">
     <div class="l-container">
         <div class="c-event-cta__inner">
@@ -27,7 +18,6 @@
                         alt="イベント情報">
                 </div>
 
->>>>>>> Stashed changes
             </a>
         </div>
     </div>
