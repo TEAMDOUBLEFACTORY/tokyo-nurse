72a0120df4879dfccb96970faaf0999bfe0b8b03
Okutani <okutani@double-factory.com>
2025-10-17 17:49:56 +0900

    commit

---
+++ page/page-society.php
@@ -0,0 +1,117 @@
+<section class="p-society" id="society">
+    <div class="p-society__sec c-sec" id="opencampus">
+        <div>
+            <img class="p-society__mainImg"
+                src="<?php echo get_template_directory_uri(); ?>/assets/images/lower/society/challenge.jpg"
+                alt="もう一度、本当の夢へチャレンジしよう！">
+            <img class="p-society__arrow"
+                src="<?php echo get_template_directory_uri(); ?>/assets/images/lower/parents/parent_img03.svg" alt="">
+        </div>
+        <h2 class="p-society__title u-color-main u-text-center u-f32">
+            再進学を支える<span class="p-society__titleNum">5</span><span
+                class="p-society__titleBold">つ</span>のポイント<span>！</span>
+        </h2>
+        <div>
+            <ul class="c-point__list">
+                <li class="c-point__item">
+                    <span class="c-point__deco">
+                        POINT <b>01</b>
+                    </span>
+                    <div class="c-point__content">
+                        <span class="u-color-main">社会人・大学生・短大生・フリーター出身者が多く入学しています。</span>
+                        <p>
+                            社会人・大学生・短大生・フリーターだった方々など、<br>皆さんと同じ夢を持って、再進学をされる方はたくさんいますので、ご安心ください。
+                        </p>
+                    </div>
+                </li>
+                <li class="c-point__item">
+                    <span class="c-point__deco">
+                        POINT <b>02</b>
+                    </span>
+                    <div class="c-point__content">
+                        <span class="u-color-main">再進学者専用の特待生制度を設置！</span>
+                        <p>
+                            再進学者のみが受験可能な「高校既卒・社会人入学（特待生入学制度あり）」があります。<br>その他にも様々な入試方法がありますので自分に合った入試方法で受験をすることができます
+                        </p>
+                        <img class="p-society__scholarshipImg"
+                            src="<?php echo get_template_directory_uri(); ?>/assets/images/lower/society/scholarship.jpg"
+                            alt="">
+                        <div class="info-item__btn c-card02--btn p-society__btn p-society__btn02">
+                            <a href="" accesskey="" class="c-btn__link">出願方法について</a>
+                        </div>
+                    </div>
+                </li>
+                <ul class="c-point__list">
+                    <li class="c-point__item">
+                        <span class="c-point__deco">
+                            POINT <b>03</b>
+                        </span>
+                        <div class="c-point__content">
+                            <span class="u-color-main">学費サポートが充実！</span>
+                            <ul>
+                                <li>
+                                    <p class="p-society__itemText">学費は、前期・後期と分納することができます。</p>
+                                </li>
+
+                                <li>
+                                    <p class="p-society__itemText">日本学生支援機構奨学金制度の利用ができます。</p>
+                                    <p class="p-society__itemPl">専門学校在学中に毎月2万円～12万円の貸与・給付を受けることができます。</p>
+                                </li>
+
+                                <li>
+                                    <p class="p-society__itemText">日本学生支援機構第二種奨学金を利用した学費分割払い制度が利用できます。</p>
+                                    <p class="p-society__itemPl">入学金10万円と教材実習費及びその他の費用のみの納入で入学することができます。</p>
+                                </li>
+
+                                <li>
+                                    <p class="p-society__itemText">新しい修学支援制度を活用した三幸学園給付奨学金&学費納入制度が利用できます。</p>
+                                </li>
+
+                                <li>
+                                    <p class="p-society__itemText">国の教育ローン・三幸学園提携教育ローンを利用することができます。</p>
+                                </li>
+                            </ul>
+                            <div class="info-item__btn c-card02--btn p-society__btn p-society__btn03">
+                                <a href="" accesskey="" class="c-btn__link">学費サポート制度について</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="c-point__item">
+                        <span class="c-point__deco">
+                            POINT <b>04</b>
+                        </span>
+                        <div class="c-point__content">
+                            <span class="u-color-main">就職サポートが充実！</span>
+                            <p>
+                                就職のためのしっかりサポート確かなフォローで、希望の就職先へ医療機関への就職について詳しいキャリア支援担当が的確にアドバイス。病院での実務経験
+                                があるベテラン教員が就職活動中の悩み相談にも乗り、徹底的にサポートします。
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="c-point__list">
+                    <li class="c-point__item">
+                        <span class="c-point__deco">
+                            POINT <b>05</b>
+                        </span>
+                        <div class="c-point__content">
+                            <span class="u-color-main">既に高校を卒業された方もオープンキャンパス・学校説明会に参加できる！</span>
+                            <p>
+                                既に高校を卒業された方でも参加可能です。進学後のことや、入学前に不安な学費、入試のことなど個別に相談も可能です。
+                            </p>
+                            <div class="p-society__bgWhite">
+                                <img class="p-society__calendar"
+                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/lower/society/event_calendar.jpg"
+                                    alt="もう一度、本当の夢へチャレンジしよう！">
+                                <p class="p-society__eventText">他の日程でもイベントを開催中！</p>
+                                <div>
+                                    <a href="" class="c-btn c-btn--arrow p-society--arrowBtn">全ての日程をカレンダーでチェック</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </ul>
+        </div>
+    </div>
+</section>
